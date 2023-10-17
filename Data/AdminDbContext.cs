e567956c7a5623f12aa56777402278aa80d0d582
MokshaDill <mokshadil@gmail.com>
2023-10-18 00:09:30 +0530

    add initial

---
+++ Data/AdminDbContext.cs
@@ -0,0 +1,21 @@
+﻿using adbms_group_project.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace adbms_group_project.AdminData
+{
+
+    public class AdminDbContext : DbContext
+    {
+        public AdminDbContext() : base("DefaultConnection")
+        {
+        }
+
+
+        public DbSet<Admin> Admins { get; set; }
+    }
+
+}
